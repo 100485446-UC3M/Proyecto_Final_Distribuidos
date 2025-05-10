687259978a993d54cca33002d049721f702af948
100495966 <100495966@alumnos.uc3m.es>
2025-05-10 18:14:07 +0200

    cliente en python

---
+++ client.py
@@ -0,0 +1,223 @@
+from enum import Enum
+import argparse
+import protocol
+
+class client :
+
+    # ******************** TYPES *********************
+    # *
+    # * @brief Return codes for the protocol methods
+    # review
+    class RC(Enum) :
+        OK = 0
+        ERROR = 1
+        USER_ERROR = 2
+
+    # clase que contiene los posibles valores de la máquina de estados del cliente
+    class State(Enum):
+        UNREGISTERED = 0
+        REGISTERED   = 1
+        CONNECTED    = 2
+
+    _state = State.UNREGISTERED
+
+    # ****************** ATTRIBUTES ******************
+    _server = None
+    _port = -1
+
+    # ******************** METHODS *******************
+
+    @staticmethod
+    def  register(user) :
+        if (client._state == client.State.UNREGISTERED) and (user is not None) and (type(user) is str) and (0 < len(user.encode("utf-8")) <= protocol.MAX_LEN):  
+            msg = protocol.register(client._server, client._port, user)
+            print(msg)
+            if msg == "REGISTER OK":
+                client._state = client.State.REGISTERED
+        else:
+            settings = protocol.SETTINGS['register']
+            print(settings[settings['default']])
+   
+    @staticmethod
+    def  unregister(user) :
+        if (client._state != client.State.UNREGISTERED) and (user is not None) and (type(user) is str) and (0 < len(user.encode("utf-8")) <= protocol.MAX_LEN):  
+            msg = protocol.unregister(client._server, client._port, user)
+            print(msg)
+            if msg == "UNREGISTER OK":
+                client._state = client.State.UNREGISTERED
+        else:
+            settings = protocol.SETTINGS['unregister']
+            print(settings[settings['default']])
+   
+
+    
+    @staticmethod
+    def  connect(user) :
+        if (client._state == client.State.REGISTERED) and (user is not None) and (type(user) is str) and (0 < len(user.encode("utf-8")) <= protocol.MAX_LEN):  
+            msg = protocol.connect(client._server, client._port, user)
+            print(msg)
+            if msg == "CONNECT OK":
+                client._state = client.State.CONNECTED
+        else:
+            settings = protocol.SETTINGS['connect']
+            print(settings[settings['default']])
+
+
+    @staticmethod
+    def  disconnect(user) :
+        #  todo
+        return client.RC.ERROR
+    
+
+    @staticmethod
+    def  publish(fileName,  description) :
+        #  todo
+        return client.RC.ERROR
+
+    @staticmethod
+    def  delete(fileName) :
+        #  todo
+        return client.RC.ERROR
+    
+
+
+    @staticmethod
+    def  listusers() :
+        #  todo
+        return client.RC.ERROR
+
+    @staticmethod
+    def  listcontent(user) :
+        #  todo
+        return client.RC.ERROR
+    
+
+    @staticmethod
+    def  getfile(user,  remote_FileName,  local_FileName) :
+        #  todo
+        return client.RC.ERROR
+
+    # *
+    # **
+    # * @brief Command interpreter for the client. It calls the protocol functions.
+    @staticmethod
+    def shell():
+
+        while (True) :
+            try :
+                command = input("c> ")
+                line = command.split(" ")
+                if (len(line) > 0):
+
+                    line[0] = line[0].upper()
+
+                    if (line[0]=="REGISTER") :
+                        if (len(line) == 2) :
+                            client.register(line[1])
+                        else :
+                            print("Syntax error. Usage: REGISTER <userName>")
+
+                    elif(line[0]=="UNREGISTER") :
+                        if (len(line) == 2) :
+                            client.unregister(line[1])
+                        else :
+                            print("Syntax error. Usage: UNREGISTER <userName>")
+
+                    elif(line[0]=="CONNECT") :
+                        if (len(line) == 2) :
+                            client.connect(line[1])
+                        else :
+                            print("Syntax error. Usage: CONNECT <userName>")
+                    
+                    elif(line[0]=="PUBLISH") :
+                        if (len(line) >= 3) :
+                            #  Remove first two words
+                            description = ' '.join(line[2:])
+                            client.publish(line[1], description)
+                        else :
+                            print("Syntax error. Usage: PUBLISH <fileName> <description>")
+
+                    elif(line[0]=="DELETE") :
+                        if (len(line) == 2) :
+                            client.delete(line[1])
+                        else :
+                            print("Syntax error. Usage: DELETE <fileName>")
+
+                    elif(line[0]=="LIST_USERS") :
+                        if (len(line) == 1) :
+                            client.listusers()
+                        else :
+                            print("Syntax error. Use: LIST_USERS")
+
+                    elif(line[0]=="LIST_CONTENT") :
+                        if (len(line) == 2) :
+                            client.listcontent(line[1])
+                        else :
+                            print("Syntax error. Usage: LIST_CONTENT <userName>")
+
+                    elif(line[0]=="DISCONNECT") :
+                        if (len(line) == 2) :
+                            client.disconnect(line[1])
+                        else :
+                            print("Syntax error. Usage: DISCONNECT <userName>")
+
+                    elif(line[0]=="GET_FILE") :
+                        if (len(line) == 4) :
+                            client.getfile(line[1], line[2], line[3])
+                        else :
+                            print("Syntax error. Usage: GET_FILE <userName> <remote_fileName> <local_fileName>")
+
+                    elif(line[0]=="QUIT") :
+                        if (len(line) == 1) :
+                            break
+                        else :
+                            print("Syntax error. Use: QUIT")
+                    else :
+                        print("Error: command " + line[0] + " not valid.")
+            except Exception as e:
+                print("Exception: " + str(e))
+
+    # *
+    # * @brief Prints program usage
+    @staticmethod
+    def usage() :
+        print("Usage: python3 client.py -s <server> -p <port>")
+
+
+    # *
+    # * @brief Parses program execution arguments
+    @staticmethod
+    def  parseArguments(argv) :
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-s', type=str, required=True, help='Server IP')
+        parser.add_argument('-p', type=int, required=True, help='Server Port')
+        args = parser.parse_args()
+
+        if (args.s is None):
+            parser.error("Usage: python3 client.py -s <server> -p <port>")
+            return False
+
+        if ((args.p < 1024) or (args.p > 65535)):
+            parser.error("Error: Port must be in the range 1024 <= port <= 65535");
+            return False
+        
+        client._server = args.s
+        client._port = args.p
+
+        return True
+
+
+    # ******************** MAIN *********************
+    @staticmethod
+    def main(argv) :
+        if (not client.parseArguments(argv)) :
+            client.usage()
+            return
+        # todo: El nombre server puede ser tanto el nombre (dominio-punto) como la dirección IP (decimal-punto) del servidor.
+
+        client.shell()
+        print("+++ FINISHED +++")
+    
+
+if __name__=="__main__":
+    client.main([])
---
+++ protocol.py
@@ -0,0 +1,175 @@
+import socket, threading
+
+MAX_LEN = 256  # tamaño máximo de nombre, ruta, etc.
+
+
+# Configuración para operaciones
+SETTINGS = {
+    'register': {
+        0: "REGISTER OK",
+        1: "USERNAME IN USE",
+        2: "REGISTER FAIL",
+        'default': 2
+    },
+    'unregister': {
+        0: "UNREGISTER OK",
+        1: "USER DOES NOT EXIST",
+        2: "UNREGISTER FAIL",
+        'default': 2
+    },
+    'connect': {
+        0: "CONNECT OK",
+        1: "CONNECT FAIL , USER DOES NOT EXIST", # review: espacio antes de la coma
+        2: "USER ALREADY CONNECTED",
+        3: "CONNECT FAIL",
+        'default': 3
+    },
+    'publish': {
+        0: "PUBLISH OK",
+        1: "PUBLISH FAIL , USER DOES NOT EXIST",
+        2: "PUBLISH FAIL , USER NOT CONNECTED",
+        3: "PUBLISH FAIL , CONTENT ALREADY PUBLISHED",
+        4: "PUBLISH FAIL",
+        'default': 4
+    },
+    'delete': {
+        0: "DELETE OK",
+        1: "DELETE FAIL , USER DOES NOT EXIST",
+        2: "DELETE FAIL , USER NOT CONNECTED",
+        3: "DELETE FAIL , CONTENT NOT PUBLISHED",
+        4: "DELETE FAIL",
+        'default': 4
+    },
+    'list_users': {
+        0: "LIST_USERS OK",
+        1: "LIST_USERS FAIL , USER DOES NOT EXIST",
+        2: "LIST_USERS FAIL , USER NOT CONNECTED",
+        3: "LIST_USERS FAIL",
+        'default': 3
+    },
+    'list_content': {
+        0: "LIST_CONTENT OK",
+        1: "LIST_CONTENT FAIL , USER DOES NOT EXIST",
+        2: "LIST_CONTENT FAIL , USER NOT CONNECTED",
+        3: "LIST_CONTENT FAIL , REMOTE USER DOES NOT EXIST",
+        4: "LIST_CONTENT FAIL",
+        'default': 4
+    },
+    'disconnect': {
+        0: "DISCONNECT OK",
+        1: "DISCONNECT FAIL , USER DOES NOT EXIST",
+        2: "DISCONNECT FAIL , USER NOT CONNECTED",
+        3: "DISCONNECT FAIL",
+        'default': 3
+    },
+    'get_file': {
+        0: "GET_FILE OK",
+        1: "GET_FILE FAIL , FILE NOT EXIST",
+        2: "GET_FILE FAIL",
+        'default': 2
+    }
+}
+
+def send_str(sock: socket.socket, txt: str) -> None:
+    # data es un objeto bytes que codifica los carácteres del string según utf-8
+    data = txt.encode('utf-8')
+    # review: no enviar string si está vacío?
+    if len(data) == 0:
+        raise ValueError("El campo está vacío")
+    elif len(data) > MAX_LEN:
+        raise ValueError(f'El campo supera {MAX_LEN} bytes: {txt!r}')
+    # crea un objeto bytes con los bytes de ambos
+    sock.sendall(data + b'\0')
+
+def recv_str(sock: socket.socket) -> str:
+    # como el objeto bytes, pero un bytearray es mutable
+    buf = bytearray()
+    while True:
+        # b es un objeto bytes que contiene un único byte
+        b = sock.recv(1)
+        if not b:
+            raise ConnectionError('Conexión cerrada inesperadamente')
+        # b'\0' es un objeto bytes que contiene el byte \0
+        if b == b'\0':
+            break
+        buf.extend(b)
+        if len(buf) > MAX_LEN:
+            raise ValueError('Recibido demasiado largo')
+    # una vez estamos seguros de tener todo el string, lo decodificamos y devolvemos
+    return buf.decode('utf-8')
+
+def recv_byte(sock: socket.socket) -> int:
+    b = sock.recv(1)
+    if not b:
+        raise ConnectionError('No se recibió el código de resultado')
+    # acceder a un elemento del objeto bytes devuelve el valor entero de ese byte
+    return b[0]
+
+def communicate_with_server(server: str, port: int, list_str: list, default_error_value: int) -> int:
+    try:
+        # with asegura cerrar el socket después de salir de él
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # timeout de 5 segundos para que si hay un fallo en las operaciones bloqueantes, el programa no quede bloqueado indefinidamente
+            sock.settimeout(5)
+            sock.connect((server, port))
+            # envíamos todas las cadenas necesarias al servidor
+            for string in list_str:
+                send_str(sock, string)
+            # devolvemos el código recibido
+            return recv_byte(sock)
+            
+    # si hay cualquier tipo de error en el cliente, se devuelve el valor predeterminado de error
+    except (socket.error, ValueError, ConnectionError, OSError, TimeoutError, UnicodeError) as e:
+        return default_error_value
+    
+def register(server: str, port: int, user: str) -> int:
+    settings = SETTINGS['register']
+    code = communicate_with_server(server, port, ["REGISTER", user], settings['default'])
+    return settings.get(code, settings[settings['default']])
+
+def unregister(server: str, port: int, user: str) -> int:
+    settings = SETTINGS['unregister']
+    code = communicate_with_server(server, port, ["UNREGISTER", user], settings['default'])
+    return settings.get(code, settings[settings['default']])
+
+def connect(server: str, port: int, user: str) -> int:
+    # Creamos el socket de servidor
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Configuramos este socket con la IP host local y puerto 0 → el SO elige un puerto libre
+    listener.bind(('0.0.0.0', 0))
+
+    # Recuperamos el puerto asignado
+    _, chosen_port = listener.getsockname()
+    print(f"Puerto libre asignado: {chosen_port}")
+
+    # Empieza a escuchar. Permitimos que haya hasta 5 clientes en cola si ya hay uno conectado
+    listener.listen(5)
+
+    # función del hilo que escucha las peticiones de descarga de ficheros de otros usuarios
+    def p2p(sock):
+        while True:
+            # connection es un nuevo socket que se usa para transmitir datos con el otro cliente
+            # client adress es una tupla con la dirección ip y el puerto del cliente
+            connection, client_address = sock.accept()
+            
+            # todo: manejar petición
+            try:
+                pass
+            finally:
+                connection.close()
+
+    # todo: cerrar sock (socket de servidor). 
+    # qué ocurre si el usuario hace quit en mitad de un connect?
+    # y si hay algún error en la función del thread que se encarga de escuchar y atender las peticiones
+
+    # creamos el hilo
+    t = threading.Thread(target=p2p, name='Daemon', args=(listener,))
+    t.start()
+
+    # mandamos solicitud de conexión al servidor
+    settings = SETTINGS['connect']
+    code = communicate_with_server(server, port, ["CONNECT", user, str(chosen_port)], settings['default'])
+    return settings.get(code, settings[settings['default']])
+    
